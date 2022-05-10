ca07ee93525b1530f940aeca26bd54f008fd864f
Danish-Asghar2909 <61777716+Danish-Asghar2909@users.noreply.github.com>
2022-05-10 12:27:14 +0530

    Add files via upload

---
+++ dial.ml
@@ -0,0 +1,5 @@
+<Response>
+    <Dial record="true">
+        <Number statusCallback="https://dev.vibtree.com/api/status" statusCallbackEvent="initiated, ringing, answered, completed" statusCallbackMethod="POST">918100409205</Number>
+    </Dial>
+</Response>
